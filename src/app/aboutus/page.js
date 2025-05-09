60ba23c7fdb94defdf09e3fe584f98246df5812d
pankaj1915 <pankusingh1133@gmail.com>
2025-05-09 16:55:50 +0530

    seooverview

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -2,8 +2,8 @@ import AboutusContainer from '@/containers/AboutusContainer/AboutusContainer'
 import React from 'react'
 
 export const metadata = {
-  title: " About QuantumCrafters Studio – Innovators in IT Training & Solutions",
-  description: "   Discover QuantumCrafters Studio’s journey as a leading IT company offering expert digital solutions and skill-based training in development, marketing, and emerging tech.",
+  title: " About QuantumCrafters Studio - Innovators in IT Training & Solutions",
+  description: "   Discover QuantumCrafters Studio's journey as a leading IT company offering expert digital solutions and skill-based training in development, marketing, and emerging tech.",
 };
 
 const page = () => {
