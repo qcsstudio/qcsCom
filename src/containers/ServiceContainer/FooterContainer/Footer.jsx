060bf387a228483ac335b2f42580e37185cb4d4f
qcsstudio <qcraftstudio@gmail.com>
2025-07-02 13:55:03 +0530

    pankaj/changelink

--- src/containers/ServiceContainer/FooterContainer/Footer.jsx
+++ src/containers/ServiceContainer/FooterContainer/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
                   <li><Link href="https://www.linkedin.com/company/qcsstudio">LinkedIn</Link></li>
                   <li><Link href="https://www.facebook.com/qcsstudio">Facebook</Link></li>
                   <li><Link href="https://www.instagram.com/qcsstudio">Instagram</Link></li>
-                  <li><Link href="https://www.youtube.com/@QCS-IT">YouTube</Link></li>
+                  <li><Link href="https://www.youtube.com/@qcsstudio">YouTube</Link></li>
                   <li><Link href="https://in.pinterest.com/qcsstudio">Pinterest</Link></li>
                 </ul>
               </div>
