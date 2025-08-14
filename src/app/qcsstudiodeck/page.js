c8240fb10a16554534f9c8ed0a992496a6845bb3
pankajqcs <pankajqcss@gmail.com>
2025-08-14 16:52:32 +0530

    addpdf

---
+++ src/app/qcsstudiodeck/page.js
@@ -0,0 +1,17 @@
+const page =()=> {
+return(
+    <>
+    <div style={{ height: '100vh' }}>
+      <iframe
+        src="/resources/QCSSTUDIO_Deck.pdf"
+        width="100%"
+        height="100%"
+        style={{ border: 'none' }}
+      />
+    </div>
+
+    </>
+)
+
+}
+export default page
