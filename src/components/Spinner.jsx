bd30ff64e49f0197204483fe694e9a4a09b06f3f
pankajqcs <pankajqcss@gmail.com>
2025-09-05 16:49:15 +0530

    pankaj/pdfloader

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className="w-6 h-6 border-4 border-purple-500 border-t-transparent rounded-full animate-spin"/>
+  )
+}
+
+export default Spinner
