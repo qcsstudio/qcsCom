3b729f31563c3b7178c970c9035b27dbf6920210
pankajqcs <pankajqcss@gmail.com>
2025-09-05 15:47:33 +0530

    pankaj/fixpdf

--- src/components/QcsDeck/QcsDeck.jsx
+++ src/components/QcsDeck/QcsDeck.jsx
@@ -22,7 +22,7 @@ const QcsDeck = () => {
       setFormSubmitted(false);
 
       try {
-        const res = await fetch("http://localhost:3000/api/send-mail", {
+        const res = await fetch("/api/send-mail", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
